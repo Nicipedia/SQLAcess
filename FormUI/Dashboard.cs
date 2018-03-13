073c63c610afc8449eab545da8638e925ae7dc69
Nicipedia <37289174+Nicipedia@users.noreply.github.com>
2018-03-13 07:19:26 +0200

    Update of project

--- FormUI/Dashboard.cs
+++ FormUI/Dashboard.cs
@@ -12,9 +12,25 @@ namespace FormUI
 {
     public partial class Dashboard : Form
     {
+        List<Person> people = new List<Person>();
         public Dashboard()
         {
             InitializeComponent();
+
+            peopleFoundListbox.DataSource = people;
+            peopleFoundListbox.DisplayMember = "FullInfo";
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            DataAccess db = new DataAccess();
+
+           people = db.GetPeople(lastNameText.Text);
         }
     }
 }
---
+++ FormUI/DataAccess.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using System.Data;
+
+namespace FormUI
+{
+    class DataAccess
+    {
+        public List<Person> GetPeople(string lastName)
+        {
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnVal("SampleDB"))
+            {
+
+            }
+        }
+    }
+}
