fa6a804ff74b3334b66b8ff90792aef1768a93d1
Nicipedia <37289174+Nicipedia@users.noreply.github.com>
2018-03-13 07:19:16 +0200

    Update of project

---
+++ SQLAccess/FormUI/Dashboard.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace FormUI
+{
+    partial class Dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.peopleFoundListbox = new System.Windows.Forms.ListBox();
+            this.lastNameText = new System.Windows.Forms.TextBox();
+            this.lastNameLable = new System.Windows.Forms.Label();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // peopleFoundListbox
+            // 
+            this.peopleFoundListbox.FormattingEnabled = true;
+            this.peopleFoundListbox.ItemHeight = 20;
+            this.peopleFoundListbox.Location = new System.Drawing.Point(49, 99);
+            this.peopleFoundListbox.Name = "peopleFoundListbox";
+            this.peopleFoundListbox.Size = new System.Drawing.Size(243, 204);
+            this.peopleFoundListbox.TabIndex = 0;
+            this.peopleFoundListbox.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // lastNameText
+            // 
+            this.lastNameText.Location = new System.Drawing.Point(141, 27);
+            this.lastNameText.Name = "lastNameText";
+            this.lastNameText.Size = new System.Drawing.Size(151, 26);
+            this.lastNameText.TabIndex = 1;
+            // 
+            // lastNameLable
+            // 
+            this.lastNameLable.AutoSize = true;
+            this.lastNameLable.Location = new System.Drawing.Point(49, 27);
+            this.lastNameLable.Name = "lastNameLable";
+            this.lastNameLable.Size = new System.Drawing.Size(86, 20);
+            this.lastNameLable.TabIndex = 2;
+            this.lastNameLable.Text = "Last Name";
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(141, 57);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(96, 36);
+            this.searchButton.TabIndex = 3;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // Dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(426, 402);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.lastNameLable);
+            this.Controls.Add(this.lastNameText);
+            this.Controls.Add(this.peopleFoundListbox);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Dashboard";
+            this.Text = "SQL Data Acess";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox peopleFoundListbox;
+        private System.Windows.Forms.TextBox lastNameText;
+        private System.Windows.Forms.Label lastNameLable;
+        private System.Windows.Forms.Button searchButton;
+    }
+}
+
---
+++ SQLAccess/FormUI/Dashboard.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormUI
+{
+    public partial class Dashboard : Form
+    {
+        List<Person> people = new List<Person>();
+        public Dashboard()
+        {
+            InitializeComponent();
+
+            peopleFoundListbox.DataSource = people;
+            peopleFoundListbox.DisplayMember = "FullInfo";
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            DataAccess db = new DataAccess();
+
+           people = db.GetPeople(lastNameText.Text);
+        }
+    }
+}
